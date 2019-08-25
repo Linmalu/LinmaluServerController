332229e597dfac991e378fa6e94adddeba7098d4
Linmalu <gkdwns0103@naver.com>
2019-08-25 13:24:43 +0900

    1.13.2.0 Update

--- src/com/linmalu/servercontroller/MainEvent.java
+++ src/com/linmalu/servercontroller/MainEvent.java
@@ -2,11 +2,120 @@ package com.linmalu.servercontroller;
 
 import com.linmalu.library.api.LinmaluEvent;
 import com.linmalu.library.api.LinmaluMain;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 public class MainEvent extends LinmaluEvent
 {
+	private final MainConfig _config = Main.getInstance().GetMainConfig();
+
 	public MainEvent(LinmaluMain main)
 	{
 		super(main);
+	}
+
+	@EventHandler
+	public void event(FoodLevelChangeEvent event)
+	{
+		if(_config.isFoodStop())
+		{
+			event.setCancelled(true);
+		}
+	}
+
+	@EventHandler
+	public void event(BlockBreakEvent event)
+	{
+		Player player = event.getPlayer();
+		if(player.isOp())
+		{
+			return;
+		}
+		if(_config.isBlockBreakStop())
+		{
+			event.setCancelled(true);
+			player.sendMessage(Main.getInstance().getTitle() + ChatColor.YELLOW + "금지된 행동입니다.");
+		}
+	}
+
+	@EventHandler
+	public void event(BlockPlaceEvent event)
+	{
+		Player player = event.getPlayer();
+		if(player.isOp())
+		{
+			return;
+		}
+		if(_config.isBlockPlaceStop())
+		{
+			event.setCancelled(true);
+			player.sendMessage(Main.getInstance().getTitle() + ChatColor.YELLOW + "금지된 행동입니다.");
+		}
+	}
+
+	@EventHandler
+	public void event(AsyncPlayerChatEvent event)
+	{
+		Player player = event.getPlayer();
+		if(player.isOp())
+		{
+			return;
+		}
+		if(_config.isChattingStop())
+		{
+			event.setCancelled(true);
+			player.sendMessage(Main.getInstance().getTitle() + ChatColor.YELLOW + "금지된 행동입니다.");
+		}
+	}
+
+	@EventHandler
+	public void event(PlayerMoveEvent event)
+	{
+		Player player = event.getPlayer();
+		if(player.isOp())
+		{
+			return;
+		}
+		if(!_config.isMoveStop())
+		{
+			return;
+		}
+		Location fromLocation = event.getFrom();
+		Location toLocation = event.getTo();
+		if(fromLocation.getBlockX() == toLocation.getBlockX() && fromLocation.getBlockZ() == toLocation.getBlockZ())
+		{
+			return;
+		}
+		event.setCancelled(true);
+		player.sendMessage(Main.getInstance().getTitle() + ChatColor.YELLOW + "금지된 행동입니다.");
+	}
+
+	@EventHandler
+	public void event(EntityDamageByEntityEvent event)
+	{
+		Entity entity1 = event.getDamager();
+		Entity entity2 = event.getEntity();
+		if(!(entity1 instanceof Player && entity2 instanceof Player))
+		{
+			return;
+		}
+		if(entity1.isOp())
+		{
+			return;
+		}
+		if(_config.isAttackStop())
+		{
+			event.setCancelled(true);
+			entity1.sendMessage(Main.getInstance().getTitle() + ChatColor.YELLOW + "금지된 행동입니다.");
+		}
 	}
 }
