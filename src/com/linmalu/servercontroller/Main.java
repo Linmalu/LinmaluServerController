edb3b014ae64cc23f472d3ed948d1e16924514c6
Linmalu <gkdwns0103@naver.com>
2019-08-27 03:18:36 +0900

    1.13.2.0 스마트무빙 메시지

--- src/com/linmalu/servercontroller/Main.java
+++ src/com/linmalu/servercontroller/Main.java
@@ -1,9 +1,13 @@
 package com.linmalu.servercontroller;
 
 import com.linmalu.library.api.LinmaluMain;
+import org.bukkit.ChatColor;
 
 public final class Main extends LinmaluMain
 {
+	public static final String SMART_MOVING_NAME = "SmartMoving".toLowerCase();
+	public static final String SMART_MOVING_MESSAGE = ChatColor.YELLOW + "스마트무빙이 금지된 서버입니다.";
+
 	private MainConfig _mainConfig;
 
 	@Override
